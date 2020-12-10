22de3e523fbea128b3b20f4554e6ea08c5d6300e
Flamigo <rasdfatgasfdg@gmail.com>
2020-12-10 17:19:06 +0300

    Lesson1

--- src/main/java/ru/homework/java/core/Race.java
+++ src/main/java/ru/homework/java/core/Race.java
@@ -2,5 +2,5 @@ package ru.homework.java.core;
 
 public interface Race {
     boolean run(String name, int lengthLimit, int distance);
-    boolean jump(String name, int heightLimit, int height);
+    boolean jump(String name, double heightLimit, double height);
 }
